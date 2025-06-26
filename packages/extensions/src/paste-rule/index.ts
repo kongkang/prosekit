b0a25ed2c56d118995de7450c60bc3d299f4fb8f
ocavue <ocavue@gmail.com>
2025-06-26 17:14:18 +1000

    split definePasteRulePlugin

--- packages/extensions/src/paste-rule/index.ts
+++ packages/extensions/src/paste-rule/index.ts
@@ -1,28 +1,8 @@
-import {
-  defineFacet,
-  defineFacetPayload,
-  pluginFacet,
-  type PlainExtension,
-  type PluginPayload,
-} from '@prosekit/core'
+import type { PlainExtension } from '@prosekit/core'
 import type { Slice } from '@prosekit/pm/model'
-import {
-  PluginKey,
-  ProseMirrorPlugin,
-} from '@prosekit/pm/state'
 import type { EditorView } from '@prosekit/pm/view'
 
-/**
- * Defines an paste rule. A paste rule can be used to transform pasted or
- * dragged-and-dropped content before it is applied to the document.
- *
- * @param options
- *
- * @public
- */
-export function definePasteRule(options: PasteRuleOptions): PlainExtension {
-  return definePasteRulePlugin(options)
-}
+import { definePasteRulePlugin } from './paste-rule-plugin'
 
 /**
  * @public
@@ -62,39 +42,13 @@ export type PasteRuleOptions = {
 }
 
 /**
- * @internal
+ * Defines an paste rule. A paste rule can be used to transform pasted or
+ * dragged-and-dropped content before it is applied to the document.
+ *
+ * @param options
+ *
+ * @public
  */
-const pasteRuleFacet = defineFacet<PasteRuleHandler, PluginPayload>({
-  reduce: () => {
-    let handlers: PasteRuleHandler[] = []
-
-    const transformPasted = (slice: Slice, view: EditorView): Slice => {
-      for (const handler of handlers) {
-        slice = handler({ slice, view })
-      }
-      return slice
-    }
-
-    const plugin = new ProseMirrorPlugin({
-      key: new PluginKey('prosekit-paste-rule'),
-      props: { transformPasted },
-    })
-
-    return (inputs: PasteRuleHandler[]) => {
-      handlers.length = 0
-      for (const input of inputs) {
-        handlers.push(input)
-      }
-      return plugin
-    }
-  },
-  singleton: true,
-  parent: pluginFacet,
-})
-
-/**
- * @internal
- */
-function definePasteRulePlugin({ handler }: PasteRuleOptions): PlainExtension {
-  return defineFacetPayload(pasteRuleFacet, [handler]) as PlainExtension
+export function definePasteRule({ handler }: PasteRuleOptions): PlainExtension {
+  return definePasteRulePlugin(handler)
 }
---
+++ packages/extensions/src/paste-rule/paste-rule-plugin.ts
@@ -0,0 +1,53 @@
+import {
+  defineFacet,
+  defineFacetPayload,
+  pluginFacet,
+  type PlainExtension,
+  type PluginPayload,
+} from '@prosekit/core'
+import type { Slice } from '@prosekit/pm/model'
+import {
+  PluginKey,
+  ProseMirrorPlugin,
+} from '@prosekit/pm/state'
+import type { EditorView } from '@prosekit/pm/view'
+
+type PasteRulePayload = (options: { slice: Slice; view: EditorView }) => Slice
+
+/**
+ * @internal
+ */
+const pasteRuleFacet = defineFacet<PasteRulePayload, PluginPayload>({
+  reduce: () => {
+    let handlers: PasteRulePayload[] = []
+
+    const transformPasted = (slice: Slice, view: EditorView): Slice => {
+      for (const handler of handlers) {
+        slice = handler({ slice, view })
+      }
+      return slice
+    }
+
+    const plugin = new ProseMirrorPlugin({
+      key: new PluginKey('prosekit-paste-rule'),
+      props: { transformPasted },
+    })
+
+    return (inputs: PasteRulePayload[]) => {
+      handlers.length = 0
+      for (const input of inputs) {
+        handlers.push(input)
+      }
+      return plugin
+    }
+  },
+  singleton: true,
+  parent: pluginFacet,
+})
+
+/**
+ * @internal
+ */
+export function definePasteRulePlugin(payload: PasteRulePayload): PlainExtension {
+  return defineFacetPayload(pasteRuleFacet, [payload]) as PlainExtension
+}
